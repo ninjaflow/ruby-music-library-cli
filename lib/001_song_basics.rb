f6458575c1304a0bcc30aa02ba46f270b638cd81
Lluvia <friasllu@gmail.com>
2023-06-14 15:19:55 -0400

    commit tree 1562a963e05370d91d117d3b2b42ba6bb9d18893

---
+++ lib/001_song_basics.rb
@@ -0,0 +1,25 @@
+class Song
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        all.clear
+    end
+
+    def save
+        self.class.all << self
+        self
+    end
+
+    def self.create(name)
+        new(name).save
+    end
+end
